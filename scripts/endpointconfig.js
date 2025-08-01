cbd21fa89f6fb888ff0ad8badee3faf575e65e37
Mark Szulc <mszulc@adobe.com>
2025-08-01 20:42:54 +1000

    Update endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p156527-e1663594.adobeaemcloud.com';
+  return 'https://publish-p137825-e1511252.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p156527-e1663594.adobeaemcloud.com';
+  return 'https://author-p137825-e1511252.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
